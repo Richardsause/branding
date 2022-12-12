9e6c8874d49fd1ca5fc5b2eefb1f6b05e39c7ac7
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-12 20:30:48 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -12,20 +12,22 @@ echo "removed all the bloat"
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf groupupdate core -y
 sudo dnf install rpmfusion-nonfree-release-tainted -y
+sudo dnf upgrade -y --alowerasing
 sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y --alowerasing
+sudo dnf groupupdate sound-and-video -y --alowerasing
 sudo dnf install intel-media-driver libva-intel-driver -y
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
 echo "rpm fusion is fully setup"
 
 # Installing some software
-sudo dnf in chromium-freeworld thunderbird kgpg thermald plasma-browser-integration ktorrent
+sudo dnf in chromium-freeworld thunderbird kgpg thermald plasma-browser-integration ktorrent -y
 sudo systemctl enable thermald
 echo "all the apps installed, and thermald has been enabled"
 
 # hblock
+sudo dnf in shasum -y
 curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.0/hblock' \
   && echo '4031d86cd04fd7c6cb1b7e9acb1ffdbe9a3f84f693bfb287c68e1f1fa2c14c3b  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
@@ -57,19 +59,6 @@ sudo dd if=/dev/zero of=/swap/swapfile bs=1M count=4096 status=progress
 sudo echo "/swap/swapfile none swap pri=0,discard 0 0" >> /etc/fstab
 echo "swap is configured"
 
-# grub
-sudo echo "GRUB_TIMEOUT=5" > /etc/default/grub
-sudo echo "GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"" >> /etc/default/grub
-sudo echo "GRUB_DEFAULT=saved" >> /etc/default/grub
-sudo echo "GRUB_DISABLE_SUBMENU=true" >> /etc/default/grub
-sudo echo "GRUB_TERMINAL_OUTPUT="console"" >> /etc/default/grub
-sudo echo "GRUB_CMDLINE_LINUX="rhgb quiet intel_pstate=disable"" >> /etc/default/grub
-sudo echo "GRUB_DISABLE_RECOVERY="true"" >> /etc/default/grub
-sudo echo "GRUB_ENABLE_BLSCFG=true" >> /etc/default/grub
-echo "new /etc/default/grub written, generating its .cfg..."
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-echo "grub has been configured"
-
 # fixing up the cursors
 mkdir ~/.icons/
 mkdir ~/.icons/default
@@ -78,8 +67,16 @@ echo "Inherits=breeze_cursors" >> ~/.icons/default/index.theme
 ln -s /usr/share/icons/breeze_cursors/cursors ~/.icons/default/cursors
 echo "default cursor theme added to plasma settings"
 
-cd "Área de trabalho"
-curl https://raw.githubusercontent.com/Richardsause/branding/main/trash%3A%E2%81%84.desktop > trash:/.desktop
+# fixing up the cursors on /etc/skel
+sudo mkdir /etc/skel/.icons/
+sudo mkdir /etc/skel/.icons/default
+sudo echo "[Icon Theme]" > /etc/skel/.icons/default/index.theme
+sudo echo "Inherits=breeze_cursors" >> /etc/skel/.icons/default/index.theme
+sudo ln -s /usr/share/icons/breeze_cursors/cursors /etc/skel/.icons/default/cursors
+echo "default cursor theme added to plasma settings on /etc/skel"
+
+cd ~/"Área de trabalho"
+curl https://raw.githubusercontent.com/Richardsause/branding/main/trash%3A%E2%81%84.desktop > ~/"Área de trabalho"/"Lixeira.desktop"
 echo "added trashbin file to the desktop"
 
 ## Branding
@@ -93,6 +90,7 @@ sudo mv BreezeRRT.colors /usr/share/color-schemes/
 sudo mv 169.png /usr/share/wallpapers/
 chmod +x wallpaper.sh
 ./wallpaper.sh
-plasma-apply-colorscheme BreezeRRT.colors
+plasma-apply-colorscheme BreezeRRT
+/usr/lib/plasma-changeicons breeze-dark
 echo "branding downloaded, and applied, removing stuff in ~/Downloads"
 sudo rm -rf ~/Downloads/*
