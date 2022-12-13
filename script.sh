599953a3ef0cdba342bad72649696ef7d5c17217
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-12 21:28:35 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 ## DNF related stuff
 sudo dnf up -y
-notify-send "updated the system"
+echo "updated the system"
 
 # Removing software
 sudo dnf rm *akonadi* *akregator* *games* *krfb* *krdc* kwrite *konversation* *abrt* *firefox* *qt5-qdbusviewer* *power-profiles-daemon* -y
-notify-send "removed all the bloat"
+echo "removed all the bloat"
 
 # Installing RPM fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
@@ -19,42 +19,42 @@ sudo dnf groupupdate sound-and-video -y --allowerasing
 sudo dnf install intel-media-driver libva-intel-driver -y
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
-notify-send "rpm fusion is fully setup"
+echo "rpm fusion is fully setup"
 
 # Installing some software
-sudo dnf in chromium-freeworld plasma-thunderbird kgpg thermald plasma-browser-integration ktorrent -y
+sudo dnf in chromium-freeworld thunderbird kgpg thermald plasma-browser-integration ktorrent -y
 sudo systemctl enable thermald
-notify-send "all the apps installed, and thermald has been enabled"
+echo "all the apps installed, and thermald has been enabled"
 
 # wine
 sudo dnf in wine vulkan-loader vulkan-loader.i686 -y
 wine a
-notify-send "wine setup and ready to use "
+echo "wine setup and ready to use "
 
 # hblock
-sudo dnf in shasum -y
+sudo dnf in perl-Digest-SHA -y
 curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.0/hblock' \
   && echo '4031d86cd04fd7c6cb1b7e9acb1ffdbe9a3f84f693bfb287c68e1f1fa2c14c3b  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
   && sudo chown 0:0 /usr/local/bin/hblock \
   && sudo chmod 755 /usr/local/bin/hblock
 sudo systemctl enable hblock.timer
-notify-send "hblock is working"
+echo "hblock is working"
 
 # auto-cpufreq
 sudo dnf in plasma-discover-snap snapd -y
 sudo snap install auto-cpufreq -y
 sudo systemctl enable auto-cpufreq
-notify-send "auto-cpufreq is working"
+echo "auto-cpufreq is working"
 
 ## Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
-flatpak update -y
-flatpak install org.gtk.Gtk3theme.Breeze -y
-flatpak override --filesystem=xdg-config/gtk-3.0:ro
-flatpak override --filesystem=xdg-config/gtk-4.0:ro
-flatpak override --filesystem=xdg-config/gtk-2.0:ro
-notify-send "flatpak is configured"
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+sudo flatpak update -y
+sudo flatpak install org.gtk.Gtk3theme.Breeze -y
+sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro
+sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro
+sudo flatpak override --filesystem=xdg-config/gtk-2.0:ro
+echo "flatpak is configured"
 
 ## Fixing the system
 sudo systemctl disable systemd-oomd
@@ -62,7 +62,7 @@ sudo systemctl disable systemd-oomd
 sudo chattr +C /swap/
 sudo dd if=/dev/zero of=/swap/swapfile bs=1M count=4096 status=progress
 sudo echo "/swap/swapfile none swap pri=0,discard 0 0" >> /etc/fstab
-notify-send "swap is configured"
+echo "swap is configured"
 
 # fixing up the cursors
 mkdir ~/.icons/
@@ -70,8 +70,7 @@ mkdir ~/.icons/default
 echo "[Icon Theme]" > ~/.icons/default/index.theme
 echo "Inherits=breeze_cursors" >> ~/.icons/default/index.theme
 ln -s /usr/share/icons/breeze_cursors/cursors ~/.icons/default/cursors
-plasma-apply-cursortheme default
-notify-send "default cursor added and applied for the current user"
+echo "default cursor theme added to plasma settings"
 
 # fixing up the cursors on /etc/skel
 sudo mkdir /etc/skel/.icons/
@@ -79,11 +78,11 @@ sudo mkdir /etc/skel/.icons/default
 sudo echo "[Icon Theme]" > /etc/skel/.icons/default/index.theme
 sudo echo "Inherits=breeze_cursors" >> /etc/skel/.icons/default/index.theme
 sudo ln -s /usr/share/icons/breeze_cursors/cursors /etc/skel/.icons/default/cursors
-notify-send "default cursor theme added to plasma settings on /etc/skel"
+echo "default cursor theme added to plasma settings on /etc/skel"
 
 cd ~/"Área de trabalho"
 curl https://raw.githubusercontent.com/Richardsause/branding/main/trash%3A%E2%81%84.desktop > ~/"Área de trabalho"/"Lixeira.desktop"
-notify-send "added trashbin file to the desktop"
+echo "added trashbin file to the desktop"
 
 ## Branding
 cd ~/Downloads
@@ -98,6 +97,5 @@ chmod +x wallpaper.sh
 ./wallpaper.sh
 plasma-apply-colorscheme BreezeRRT
 /usr/lib/plasma-changeicons breeze-dark
-notify-send "all the branding was downloaded and is waiting to be applied, doing a cleanup..."
+echo "branding downloaded, and applied, removing stuff in ~/Downloads"
 sudo rm -rf ~/Downloads/*
-systemsettings5
