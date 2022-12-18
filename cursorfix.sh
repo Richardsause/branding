1c88f536a5ca49e168d9b037b1079c14cb891019
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-18 13:39:52 -0300

    Update cursorfix.sh

--- cursorfix.sh
+++ cursorfix.sh
@@ -4,5 +4,7 @@ mkdir -p ~/.local/share/icons/default
 sudo echo "[Icon Theme]
 Inherits=breeze_cursors" > ~/.local/share/icons/default/index.theme
 sudo ln -s /usr/share/icons/breeze_cursors/cursors ~/.local/share/icons/default/
+mkdir ~/.icons
+cp ~/.local/share/icons/default -r ~/.icons/
 plasma-apply-cursortheme default
 #notify-send "cursor fix applied to current user"
