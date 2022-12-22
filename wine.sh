6f7fa63d5d7299f94c2ccab156c70813a0ce4e9e
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-22 17:08:03 -0300

    Add files via upload

--- wine.sh
+++ wine.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-rpm-ostree install wine vulkan-loader.i686
-rpm-ostree apply-live --allow-replacement
+flatpak install org.winehq.Wine/x86_64/stable-22.08 org.winehq.Wine.gecko/x86_64/stable-22.08 org.winehq.Wine.mono/x86_64/stable-22.08 -y
 #notify-send "wine has been installed and configured in the system, please reboot"
