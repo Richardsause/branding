7db2e37a3ad1e83291bb888ab1234f6a95dfe3d7
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-22 17:13:44 -0300

    Delete wine.sh

--- wine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-flatpak install org.winehq.Wine/x86_64/stable-22.08 org.winehq.Wine.gecko/x86_64/stable-22.08 org.winehq.Wine.mono/x86_64/stable-22.08 -y
-#notify-send "wine has been installed and configured in the system, please reboot"
