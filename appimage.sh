297ffeaef5e47edc07cd6672865384ae34a171ba
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-30 13:48:33 -0300

    Update appimage.sh

--- appimage.sh
+++ appimage.sh
@@ -2,4 +2,5 @@
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 sudo dnf in -y ./appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 mkdir -p ~/.config
+mkdir -p ~/Applications
 mv appimagelauncher.cfg ~/.config/appimagelauncher.cfg
