bb8c53c49a4bcb7ec25c3637fee14adb503b05e0
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-30 17:08:15 -0300

    Update manuel.sh

--- manuel.sh
+++ manuel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp Manual.odt ~/"Área de trabalho"/
+cp Manual.odt ~/"Área de trabalho"/"Manual de instruções.odt"
 sudo mkdir -p /etc/skel/"Área de trabalho"/
-sudo cp Manual.odt /etc/skel/"Área de trabalho"/
+sudo cp Manual.odt /etc/skel/"Área de trabalho"/"Manual de instruç
