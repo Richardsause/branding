0ba48ee3400d92769298fb09ff67606b70e5ef05
Richardsause <89277764+Richardsause@users.noreply.github.com>
2022-12-31 00:54:03 -0300

    Update debloat.sh

--- debloat.sh
+++ debloat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo dnf rm akonadi* kwrite akregator* konversation* dragon* *games* kmag* kmousetool* kmouth* krdc* krfb* ktnef* *dbusviewer* *abrt* kfind* korganizer* firefox* elisa* -y
+sudo dnf rm akonadi* kwrite akregator* konversation* dragon* *games* kmag* kmousetool* kmouth* krdc* krfb* ktnef* *dbusviewer* *abrt* kfind* korganizer* firefox* elisa* ibus -y
